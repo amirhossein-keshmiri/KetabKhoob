738c3f6be343963ddeca9a6ea04f9e27d6362fa1
amirhossein keshmiri <amirhosseinky.dev@gmail.com>
2023-11-26 23:19:41 +0330

    Complete Domain | Add Comment Aggregate & Banner Aggregate & Slider Aggregate & Category Aggregate and Add BaseRepository

---
+++ Shop/Shop.Domain/SiteEntities/Enums/BannerPosition.cs
@@ -0,0 +1,11 @@
+﻿namespace Shop.Domain.SiteEntities.Enums
+{
+    public enum BannerPosition
+    {
+        زیر_اسلایدر,
+        سمت_چپ_اسلایدر,
+        بالای_اسلایدر,
+        سمت_راست_شگفت_انگیز,
+        وسط_صفحه
+    }
+}
