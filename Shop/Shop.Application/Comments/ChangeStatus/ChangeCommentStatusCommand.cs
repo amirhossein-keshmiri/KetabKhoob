45a6c16fc47acb7fe0ca50a601841b4ed12f5f40
amirhossein keshmiri <amirhosseinky.dev@gmail.com>
2023-12-02 22:58:33 +0330

    Implement Comment Use Case

---
+++ Shop/Shop.Application/Comments/ChangeStatus/ChangeCommentStatusCommand.cs
@@ -0,0 +1,7 @@
+﻿using Common.Application;
+using Shop.Domain.CommentAgg.Enums;
+
+namespace Shop.Application.Comments.ChangeStatus
+{
+    public record ChangeCommentStatusCommand(long Id,CommentStatus Status):IBaseCommand;
+}
