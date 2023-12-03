99662af4265f97abaa5c83e57b14f7af8bfd7186
amirhossein keshmiri <amirhosseinky.dev@gmail.com>
2023-12-03 23:35:09 +0330

    Implement Role Use Case

---
+++ Shop/Shop.Application/Roles/Create/CreateRoleCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using Common.Application;
+using Shop.Domain.RoleAgg;
+using Shop.Domain.RoleAgg.Repository;
+
+namespace Shop.Application.Roles.Create;
+
+internal class CreateRoleCommandHandler : IBaseCommandHandler<CreateRoleCommand>
+{
+    private readonly IRoleRepository _roleRepository;
+
+    public CreateRoleCommandHandler(IRoleRepository roleRepository)
+    {
+        _roleRepository = roleRepository;
+    }
+
+    public async Task<OperationResult> Handle(CreateRoleCommand request, CancellationToken cancellationToken)
+    {
+        var permissions = new List<RolePermission>();
+        request.Permissions.ForEach(f =>
+        {
+            permissions.Add(new RolePermission(f));
+        });
+        var role = new Role(request.Title, permissions);
+        _roleRepository.Add(role);
+        await _roleRepository.Save();
+
+        return OperationResult.Success();
+    }
+}
+
---
+++ Shop/Shop.Application/Roles/Edit/EditRoleCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using Common.Application;
+using Shop.Domain.RoleAgg;
+using Shop.Domain.RoleAgg.Repository;
+
+namespace Shop.Application.Roles.Edit;
+
+internal class EditRoleCommandHandler : IBaseCommandHandler<EditRoleCommand>
+{
+    private readonly IRoleRepository _roleRepository;
+
+    public EditRoleCommandHandler(IRoleRepository roleRepository)
+    {
+        _roleRepository = roleRepository;
+    }
+
+    public async Task<OperationResult> Handle(EditRoleCommand request, CancellationToken cancellationToken)
+    {
+        var role = await _roleRepository.GetTracking(request.Id);
+        if (role == null)
+            return OperationResult.NotFound();
+
+        role.Edit(request.Title);
+
+        var permissions = new List<RolePermission>();
+        request.Permissions.ForEach(f =>
+        {
+            permissions.Add(new RolePermission(f));
+        });
+        role.SetPermission(permissions);
+        await _roleRepository.Save();
+        return OperationResult.Success();
+    }
+}
