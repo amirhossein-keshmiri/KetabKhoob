f380b2e4a9734dd7f351823e7aa57a3e4614f5a9
amirhossein keshmiri <amirhosseinky.dev@gmail.com>
2024-01-20 21:42:56 +0330

    Create Sellers & Comments Relations

---
+++ Shop/Shop.Infrastructure/Migrations/20240119204034__create_seller_relation.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shop.Infrastructure.Migrations
+{
+    public partial class _create_seller_relation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                    ALTER TABLE [seller].Sellers
+                    ADD CONSTRAINT FK_Sellers_Users_UserId
+                    FOREIGN KEY (UserId) REFERENCES [user].Users(Id);
+                ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Shop/Shop.Infrastructure/Migrations/20240120175102__create_comments_relation.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shop.Infrastructure.Migrations
+{
+    public partial class _create_comments_relation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                    ALTER TABLE [dbo].Comments
+                    ADD CONSTRAINT FK_Comments_Users_UserId
+                    FOREIGN KEY (UserId) REFERENCES [user].Users(Id);
+                ");
+
+            migrationBuilder.Sql(@"
+                    ALTER TABLE [dbo].Comments
+                    ADD CONSTRAINT FK_Comments_Products_ProductId
+                    FOREIGN KEY (ProductId) REFERENCES [product].Products(Id);
+                ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
